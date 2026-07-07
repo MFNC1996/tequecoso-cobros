d8848913d99080113e3db535480131c0d5c7f2c0
Mauricio Núñez <mfnc1996@gmail.com>
2026-07-06 23:26:51 -0400

    Update Cobro.java

--- backend/cobros/src/main/java/com/tequecoso/cobros/model/Cobro.java
+++ backend/cobros/src/main/java/com/tequecoso/cobros/model/Cobro.java
@@ -1,1 +1,41 @@
+package com.tequecoso.cobros.model;
 
+public class Cobro {
+
+    private String cliente;
+    private double monto;
+    private String estado;
+
+    public Cobro() {
+    }
+
+    public Cobro(String cliente, double monto, String estado) {
+        this.cliente = cliente;
+        this.monto = monto;
+        this.estado = estado;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
