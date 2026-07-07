09d1fe0f84c6e71f1f3c282bac09b3149fc33a38
Mauricio Núñez <mfnc1996@gmail.com>
2026-07-07 00:33:18 -0400

    Update Cobro.java

--- backend/cobros/src/main/java/com/tequecoso/cobros/model/Cobro.java
+++ backend/cobros/src/main/java/com/tequecoso/cobros/model/Cobro.java
@@ -4,14 +4,16 @@ public class Cobro {
 
     private String cliente;
     private double monto;
+    private String fechaVencimiento;
     private String estado;
 
     public Cobro() {
     }
 
-    public Cobro(String cliente, double monto, String estado) {
+    public Cobro(String cliente, double monto, String fechaVencimiento, String estado) {
         this.cliente = cliente;
         this.monto = monto;
+        this.fechaVencimiento = fechaVencimiento;
         this.estado = estado;
     }
 
@@ -29,6 +31,14 @@ public class Cobro {
 
     public void setMonto(double monto) {
         this.monto = monto;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
     }
 
     public String getEstado() {
